45b0586b37d61979a927f896297f2a13fb4028db
kalyibek <kkalyibek@gmail.com>
2022-08-19 13:32:10 +0600

    refactored code

--- main.py
+++ main.py
@@ -2,19 +2,42 @@ import os
 import sys
 import shutil
 
-dir_path = r'C:\Users\Kalyibek\PycharmProjects\pythonTasks\my_file_system'
-operation = sys.argv[1]
+def directory_list(*file, directory):
+    print(os.listdir(directory))
 
-if operation == 'list':
-    print(os.listdir(dir_path))
-else:
-    file_name = sys.argv[2]
-    file_path = os.path.join(dir_path, file_name)
-    if operation == 'add':
-        shutil.copy(file_name, dir_path)
-        print(f'{file_path} added')
-    elif operation == 'remove' and file_name in os.listdir(dir_path):
-        os.remove(file_path)
-        print(f'{file_path} deleted')
+def add_file(file, directory):
+    shutil.copy(file, directory)
+    print(f'"{file}" added')
+
+def remove_file(file, directory):
+    if file in os.listdir(directory):
+        os.remove(os.path.join(directory, file))
+        print(f'"{file}" removed')
     else:
-        print('ERROR')
+        print(f'Error: "{file}" is not in your file system.')
+
+commands = {
+    'list': directory_list,
+    'add': add_file,
+    'remove': remove_file
+}
+
+def main():
+    dir_path = r'C:\Users\Kalyibek\PycharmProjects\pythonTasks\my_file_system'
+    operation = sys.argv[1]
+    file_name = ''
+
+    if 'list' not in sys.argv:
+        if len(sys.argv) < 3:
+            exit('Enter file name')
+        else:
+            file_name = sys.argv[2]
+
+    if operation in commands:
+        commands[operation](file_name, directory=dir_path)
+    else:
+        print(f'Unknown command "{operation}"')
+
+
+if __name__ == '__main__':
+    main()
